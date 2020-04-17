c3037f1b52ba17e2cec91ac8bf24319a77f8c965
patryzina <patryzina@gmail.com>
2020-04-17 08:21:32 +0200

    mi primer commit

---
+++ plantilla angular2/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'mi-aplicacion',
+  template: '<h1>Aprende Angular 2 Fácilmente</h1>'    
+})
+
+export class AppComponent  { 
+}
